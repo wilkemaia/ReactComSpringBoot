ba7a21df3c67c8eebd43975ca5fa9fd3df39ea48
wilkemaia <wilkemaia2@gmail.com>
2021-05-04 09:57:38 -0300

    Basic Static Componentes

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/wilkemaia" target="_blank" rel="noreferrer">Wilke Rodrigues Maia</a></p>
+                <p className="text-light"><small><strong>Projeto de Estudo Spring Boot com React</strong><br />
+      Projeto Liberado no GitHub: <a href="https://github.com/wilkemaia" target="_blank" rel="noreferrer">https://github.com/wilkemaia/ReactComSpringBoot</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
