4cda1c66b3d884ebd70ab9110d2699cf651cfbff
wilkemaia <wilkemaia2@gmail.com>
2021-05-07 14:43:23 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "ultils/format";
+
+import { BASE_URL } from "ultils/requests";
+
 const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            })
+
+    }, []);
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -12,83 +37,23 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
+
+                    {
+                        page.content?.map(item => (
+                            <tr key={item.id}>
+                                <td>{formatLocalDate(item.date,'dd/MM/yyyy')}</td>
+                                <td>{item.seller.name}</td>
+                                <td>{item.visited}</td>
+                                <td>{item.deals}</td>
+                                <td>{item.amount.toFixed(2)}</td>
+                            </tr>
+
+                        ))
+
+                    }
+
+
+
                 </tbody>
             </table>
         </div>
@@ -96,3 +61,7 @@ const DataTable = () => {
 }
 
 export default DataTable;
+
+function formatLocalDatecalDate(date: string, arg1: string): import("react").ReactNode {
+    throw new Error("Function not implemented.");
+}
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,12 +1,36 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+
+export type SalePage = {
+    content?:Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size?: number;
+    number: number;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
 }
 
 export type SaleSucces = {
-    
-        sellerName: string;
-        visited: number;
-        deals: number;
-    
+
+    sellerName: string;
+    visited: number;
+    deals: number;
+
 }
